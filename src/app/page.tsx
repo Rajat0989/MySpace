bf64a9d15d1de834eb5bf92c64017ccd73b5b299
Rajat Chandarana <rajatchandarana09@gmail.com>
2024-05-12 00:12:01 +0530

    initial setup complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
-import { Button } from "@/components/ui/button"
+"use client"
 
+import { Button } from "@/components/ui/button"
+import { SignInButton, SignedIn, SignOutButton, SignedOut } from "@clerk/nextjs"
+import { api } from "../../convex/_generated/api"
+import { useMutation, useQuery } from "convex/react"
 
 export default function Home() {
+
+  const getFiles = useQuery(api.files.getFiles)
+  const createFile = useMutation(api.files.createFile)
+
   return (
-    <>
-      <Button>Click Me!</Button>
-    </>
-  );
+    <div className="h-screen w-full flex flex-col justify-around items-center">
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+
+      <Button
+        onClick={() => {
+          createFile({
+            name: "test",
+          })
+        }}
+      >
+        Click Me
+      </Button>
+    </div>
+  )
 }
